639eea3a46e03d44a9874b2cd052c7c616d79699
Jed <jamijares3@gmail.com>
2021-07-06 19:00:24 -0500

    gets the icon queue looking nice

--- Assets/Scripts/ControlQueue.cs
+++ Assets/Scripts/ControlQueue.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ControlQueue : MonoBehaviour
 {
-    [SerializeField] Queue controlQueue;
+    [SerializeField] List<Icon> controlQueue;
     [SerializeField] Icon[] controlIcons;
 
     public const int size = 5;
@@ -12,17 +12,18 @@ public class ControlQueue : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        controlQueue = new Queue();
-        AddIcon();
+        controlQueue = new List<Icon>();
+        for (int i = 0; i < 10; ++i)
+            AddIcon();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            AddIcon();
-        }
+        // if (Input.GetKeyDown(KeyCode.C))
+        // {
+        //     AddIcon();
+        // }
         if (Input.GetKeyDown(KeyCode.V))
         {
             UseIcon();
@@ -32,20 +33,32 @@ public class ControlQueue : MonoBehaviour
     public void AddIcon()
     {
         int randomIndex = Random.Range(0, controlIcons.Length);
-
         Icon newIcon = Instantiate(controlIcons[randomIndex], Vector3.zero, Quaternion.identity, this.transform);
         RectTransform rt = (RectTransform)newIcon.transform;
         rt.SetAnchor(AnchorPresets.TopLeft);
-        Vector3 position = new Vector3(rt.rect.width, -rt.rect.height, 0);
-        newIcon.transform.localPosition = position;
-        controlQueue.Enqueue(newIcon);
+        newIcon.transform.localPosition = new Vector3(rt.rect.width, -1200, 0);
+        newIcon.MoveTo(new Vector2(rt.rect.width, -rt.rect.height * controlQueue.Count));
+        controlQueue.Add(newIcon);
     }
 
     public void UseIcon()
     {
-        Icon nextIcon = (Icon)controlQueue.Dequeue();
-        nextIcon.Use();
-        // transform.position.x += (RectTransform)nextIcon.transform.
-        Destroy(nextIcon.gameObject);
+        if (controlQueue.Count > 0)
+        {
+            Icon nextIcon = (Icon)controlQueue[0];
+            nextIcon.Use();
+            controlQueue.RemoveAt(0);
+            Destroy(nextIcon.gameObject);
+
+            int i = 0;
+            foreach (Icon icon in controlQueue)
+            {
+                icon.StopAllCoroutines();
+                RectTransform rt = (RectTransform)icon.transform;
+                icon.MoveTo(new Vector2(rt.rect.width, -rt.rect.height * i));
+                ++i;
+            }
+        }
+        AddIcon();
     }
 }
--- Assets/Scripts/Icon.cs
+++ Assets/Scripts/Icon.cs
@@ -4,19 +4,51 @@ using UnityEngine;
 
 public class Icon : MonoBehaviour
 {
+    // [SerializeField] const float height = 200;
+    [SerializeField] const float moveSpeed = 800;
+    // [SerializeField] private Vector2 nextPosition;
+
+
     // Start is called before the first frame update
     private void Start()
     {
+        // MoveUp();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        Use();
+        // Use();
+        // MoveUp();
+    }
+
+    public void MoveTo(Vector3 newPosition)
+    {
+        // if (this.transform.localPosition.y < -height)
+        // {
+        //     transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        // }
+        StartCoroutine(MoveTowardsLocal(this.transform, newPosition, moveSpeed));
     }
 
     public virtual void Use()
     {
+        Debug.Log("generic icon use");
         // Destroy(gameObject);
+    }
+
+    // public void SetPosition(Vector2 newPosition)
+    // {
+    //     nextPosition = newPosition;
+
+    // }
+
+    IEnumerator MoveTowardsLocal(Transform transform, Vector3 newPosition, float speed)
+    {
+        while (transform.position != newPosition)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, newPosition, speed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
